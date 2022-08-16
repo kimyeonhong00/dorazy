6bbf791c3c452d1897184d8c847d8afdf4a2e205
kimyeonhong00 <63278864+kimyeonhong00@users.noreply.github.com>
2022-08-17 01:07:05 +0900

    알림 update
    notification activity 수정중

--- app/src/main/java/com/example/dorazy/NotificationActivity.kt
+++ app/src/main/java/com/example/dorazy/NotificationActivity.kt
@@ -5,39 +5,54 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 
 class NotificationActivity:AppCompatActivity() {
-    val CHANNEL_ID = ""
-    var textTitle = "Dorazy"
-    var textContent = "Hi"
-    var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.dorazy_circle)
-        .setContentTitle(textTitle)
-        .setContentText(textContent)
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .setAutoCancel(true)
-        .setDefaults(Notification.DEFAULT_VIBRATE)
+    private val CHANNEL_ID = "testChannel01"   // Channel for notification
+    private var notificationManager: NotificationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment1)
+        setContentView(R.layout.activity_main)
+
+        createNotificationChannel(CHANNEL_ID, "testChannel", "this is a test Channel")
+
+        val button = findViewById<Button>(R.id.noti_btn)
+        button.setOnClickListener {
+            displayNotification()
+        }
     }
-    private fun createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply{
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+
+    private fun displayNotification() {
+        val notificationId = 45
+
+        val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification.Builder(applicationContext, CHANNEL_ID)
+                .setSmallIcon(R.drawable.dorazy_circle)
+                .setContentTitle("Example")
+                .setContentText("This is Notification Test")
+                .build()
+        } else {
+            TODO("VERSION.SDK_INT < O")
         }
 
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel(channelId: String, name: String, channelDescription: String) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT // set importance
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = channelDescription
+            }
+            // Register the channel with the system
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 }
