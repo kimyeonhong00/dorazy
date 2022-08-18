570d6bebc07e750012d27b4ebcaec8d5640dd909
LIM SE JUN <lsj1137jsl@gmail.com>
2022-08-18 21:47:37 +0900

    면접실 예약 안정성 패치

--- app/src/main/java/com/example/dorazy/MeetActivity.kt
+++ app/src/main/java/com/example/dorazy/MeetActivity.kt
@@ -12,7 +12,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_profile.view.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
